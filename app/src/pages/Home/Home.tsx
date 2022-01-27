018f8d9fcedd6aa5d1da715b02ba7d266a4a82a2
Matthias Riedl <matthiasriedl@Matthiass-Mac-mini.local>
2022-01-27 13:00:41 +0100

    add testg

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from "react";
 const Home: FunctionComponent = () => {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>Hello World Test</h1>
         </>
     );
 };
