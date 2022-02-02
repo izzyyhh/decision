48c5f20ec1bcebc230ed266d889ef8ff3dbab91c
Ismail Halili <fhs44399@fh-salzburg.ac.at>
2022-02-02 11:35:45 +0100

    fix migrations

--- api/src/db/migrations/Migration20220202103222.ts
+++ api/src/db/migrations/Migration20220202103222.ts
@@ -1,17 +1,19 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20220201092734 extends Migration {
+export class Migration20220202103222 extends Migration {
 
   async up(): Promise<void> {
+    this.addSql('create table "thumbnail" ("id" uuid not null, "link" text not null);');
+    this.addSql('alter table "thumbnail" add constraint "thumbnail_pkey" primary key ("id");');
+
     this.addSql('create table "poll" ("id" uuid not null, "title" text not null, "sharelink" text not null, "owner_id" uuid not null, "type" int2 not null, "predefined" bool not null);');
     this.addSql('alter table "poll" add constraint "poll_pkey" primary key ("id");');
     this.addSql('create index "poll_owner_id_index" on "poll" ("owner_id");');
 
-    this.addSql('create table "thumbnail" ("id" uuid not null, "link" text not null);');
-    this.addSql('alter table "thumbnail" add constraint "thumbnail_pkey" primary key ("id");');
-
-    this.addSql('create table "decision" ("id" uuid not null, "user_id" uuid not null, "poll_id" uuid not null, "answer" int4 null);');
-    this.addSql('alter table "decision" add constraint "decision_pkey" primary key ("id");');
+    this.addSql('alter table "decision" add column "user_id" uuid not null, add column "poll_id" uuid not null;');
+    this.addSql('alter table "decision" drop constraint if exists "decision_answer_check";');
+    this.addSql('alter table "decision" alter column "answer" type int4 using ("answer"::int4);');
+    this.addSql('alter table "decision" alter column "answer" drop not null;');
     this.addSql('create index "decision_user_id_index" on "decision" ("user_id");');
     this.addSql('create index "decision_poll_id_index" on "decision" ("poll_id");');
 
@@ -30,6 +32,8 @@ export class Migration20220201092734 extends Migration {
     this.addSql('alter table "option" add constraint "option_poll_id_foreign" foreign key ("poll_id") references "poll" ("id") on update cascade;');
     this.addSql('alter table "option" add constraint "option_thumbnail_id_foreign" foreign key ("thumbnail_id") references "thumbnail" ("id") on update cascade;');
     this.addSql('alter table "option" add constraint "option_decision_id_foreign" foreign key ("decision_id") references "decision" ("id") on update cascade;');
+
+    this.addSql('drop table if exists "migrations_lock" cascade;');
   }
 
 }
