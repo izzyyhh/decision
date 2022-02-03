fc09f9dfe66780999c45f70f65a8a295748df5ab
stefan33maier@gmail.com <fhs44410>
2022-02-03 14:53:59 +0100

    add reading userfrom token initially

--- app/src/context/user/UserProvider.tsx
+++ app/src/context/user/UserProvider.tsx
@@ -1,10 +1,18 @@
 import { GQLUser } from "@app/graphql.generated";
+import { useAuthToken } from "@hooks/useAuthToken";
 import React, { FunctionComponent, useState } from "react";
+import { decodeToken,isExpired } from "react-jwt";
 
 import { UserContext } from "./UserContext";
 
 export const UserProvider: FunctionComponent = ({ children }) => {
-    const [user, setUser] = useState<GQLUser | null>(null);
+    const [token] = useAuthToken();
+    console.log(token)
 
+    const startUser = (token == null && isExpired(token)) ? null : {id: decodeToken(token).uid, token: token, name: ""};
+    
+    console.log({startUser});
+    const [user, setUser] = useState<GQLUser | null>(startUser);
+    console.log(user, "im state")
     return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 };
