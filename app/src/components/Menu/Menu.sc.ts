53106b9e3d4561d5aa729b0819a9eb5e92d9bbeb
web-valentin <valentin.kiefl@kiefl.de>
2022-02-04 11:22:15 +0100

    add home menu

---
+++ app/src/components/Menu/Menu.sc.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MenuWrapper = styled.div`
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: lightgrey;
+    padding: 15px 0px;
+`;
--- app/src/pages/PollWithType/PollWithType.sc.tsx
+++ app/src/pages/PollWithType/PollWithType.sc.tsx
@@ -4,6 +4,7 @@ export const CreatePollWithTypeWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(24, 1fr);
     grid-template-rows: auto;
+    margin-bottom: 50px;
 `;
 
 export const HeadlineWrapper = styled.div`
