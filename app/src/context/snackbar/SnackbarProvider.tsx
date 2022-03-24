29cc41157e1e2755ad6d7b16fae4be235f83d0e2
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2022-03-24 13:00:23 +0100

    add snack context and use provider

---
+++ app/src/context/snackbar/SnackbarProvider.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent, useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import { SnackContext } from "./SnackContext";
+import MuiAlert, { AlertColor, AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const SnackbarProvider: FunctionComponent = ({ children }) => {
+    const [snack, setSnack]: any = useState({ message: "", open: false });
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setSnack({ message: "", open: false });
+    };
+
+    return (
+        <SnackContext.Provider value={{ snack, setSnack }}>
+            {children}
+            <Snackbar open={snack.open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={snack.severity != undefined ? snack.severity : "info"} sx={{ width: "100%" }}>
+                    {snack.message}
+                </Alert>
+            </Snackbar>
+        </SnackContext.Provider>
+    );
+};
