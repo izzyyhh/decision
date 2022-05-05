cc796398627174fbcfc84e111b188e17a6d5eee6
web-valentin <valentin.kiefl@kiefl.de>
2022-05-05 15:26:51 +0200

    add option rear to qr code and include qrcode in modal

--- app/src/pages/Join/Join.sc.ts
+++ app/src/pages/Join/Join.sc.ts
@@ -3,3 +3,8 @@ import styled from "styled-components";
 export const LinkWrapper = styled.div`
     margin-top: 50px;
 `;
+
+export const QrCodeWrapper = styled.div`
+    width: min(500px, 75vw);
+    overflow-x: hidden;
+`;
--- app/src/pages/Join/Join.tsx
+++ app/src/pages/Join/Join.tsx
@@ -5,6 +5,7 @@ import Seo from "@app/seo/Seo";
 import Auth from "@components/Auth/Auth";
 import Headline from "@components/Headline/Headline";
 import LinkButton from "@components/LinkButton/LinkButton";
+import { CustomModal } from "@components/Modals/QRModal";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import { getPoll } from "@pages/DecisionPage/pollData.gql";
@@ -13,7 +14,7 @@ import { useTranslation } from "react-i18next";
 import { QrReader } from "react-qr-reader";
 import { useParams } from "react-router";
 
-import { LinkWrapper } from "./Join.sc";
+import { LinkWrapper, QrCodeWrapper } from "./Join.sc";
 
 const Join: FunctionComponent = () => {
     const { pollId } = useParams();
@@ -23,7 +24,15 @@ const Join: FunctionComponent = () => {
     const poll = useQuery<GQLQuery>(getPoll, { variables: { data: { pollId } } });
     const pollData = poll.data ? poll.data.getPoll : null;
     const [showNotification, setShowNotification] = useState<boolean>(false);
-    const [showScanner, setShowScanner] = useState<boolean>(false);
+    //const [showScanner, setShowScanner] = useState<boolean>(false);
+
+    const [openModal, setOpenModal] = useState(false);
+    const handleClickOpenModal = () => {
+        setOpenModal(true);
+    };
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
 
     const copyToClipBoard = () => {
         navigator.clipboard.writeText(`${window.location.origin}/join/${pollId}`);
@@ -56,25 +65,28 @@ const Join: FunctionComponent = () => {
                             {t("decision.linkCopied")}
                         </Alert>
                     )}
-                    <LinkButton active={true} arrow={false} primary={true} onClick={() => setShowScanner(true)}>
+                    <LinkButton active={true} arrow={false} primary={true} onClick={handleClickOpenModal}>
                         {t("join.decision.with.qrcode")}
                     </LinkButton>
-                    {showScanner && (
-                        <QrReader
-                            constraints={{ facingMode: "back" }}
-                            onResult={(result, error) => {
-                                if (result) {
-                                    if (result.getText().includes(window.location.origin)) {
-                                        window.location.href = result.getText();
+                    <CustomModal openModal={openModal} handleClose={handleCloseModal}>
+                        <QrCodeWrapper>
+                            <QrReader
+                                className="test"
+                                constraints={{ facingMode: "rear" }}
+                                onResult={(result, error) => {
+                                    if (result) {
+                                        if (result.getText().includes(window.location.origin)) {
+                                            window.location.href = result.getText();
+                                        }
                                     }
-                                }
 
-                                if (error) {
-                                    console.info(error);
-                                }
-                            }}
-                        />
-                    )}
+                                    if (error) {
+                                        console.info(error);
+                                    }
+                                }}
+                            />
+                        </QrCodeWrapper>
+                    </CustomModal>
                 </ColumnFullWidth>
             </Auth>
         </>
