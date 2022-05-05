1757d2a5ef71a8025ace2bc949b00fadfb2bca03
Matthias Riedl <matthiasriedl@Matthiass-Mac-mini.local>
2022-05-05 14:18:25 +0200

    finish modal

--- app/src/components/ShareModal/ShareModal.tsx
+++ app/src/components/ShareModal/ShareModal.tsx
@@ -1,13 +1,19 @@
+import { useQuery } from "@apollo/client";
+import { GQLQuery } from "@app/graphql.generated";
 import LinkButton from "@components/LinkButton/LinkButton";
+import { useSnack } from "@context/snackbar/useSnack";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import TelegramIcon from "@material-ui/icons/Telegram";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
-import React, { FunctionComponent } from "react";
+import LinkOutlinedIcon from "@mui/icons-material/LinkOutlined";
+import QrCode2OutlinedIcon from "@mui/icons-material/QrCode2Outlined";
+import { getQRCode } from "@pages/DecisionPage/pollData.gql";
+import React, { FunctionComponent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
 import { EmailShareButton, TelegramShareButton, WhatsappShareButton } from "react-share";
 
-import { Body, Links, Modal, ShareBar, Title } from "./ShareModal.sc";
+import { ArrowIcon, Body, Image, Links, Modal, QRCodeWrapper, ShareBar, Title } from "./ShareModal.sc";
 
 interface Props {
     pollId: string;
@@ -17,16 +23,25 @@ const ShareModal: FunctionComponent<Props> = ({ pollId }) => {
     const shareLink = `${window.location.origin}/decision/${pollId}`;
     const { t } = useTranslation();
     const navigate = useNavigate();
+    const { setSnack } = useSnack();
+    const [showQR, setShowQr] = useState<boolean>(false);
+    const qrCodeData = useQuery<GQLQuery>(getQRCode, { variables: { data: { shareLink } } });
+    const qrCodeBase64 = qrCodeData.data?.getQRCode.id;
+
+    const copyToClipBoard = () => {
+        navigator.clipboard.writeText(shareLink);
+        setSnack({ message: t("decision.linkCopied"), severity: "info", open: true });
+    };
 
     if (pollId.length < 1) {
         return <></>;
     }
 
     return (
-        <Modal open={true}>
+        <Modal open={true} onBackdropClick={() => window.location.reload()} BackdropProps={{ style: { backgroundColor: "rgba(0,0,0, 0.7)" } }}>
             <Body>
-                <Title>{t("link.social.share")}</Title>
-                <ShareBar>
+                <Title visible={showQR}>{t("link.social.share")}</Title>
+                <ShareBar visible={showQR}>
                     <WhatsappShareButton url={shareLink} title={t("link.social.join")} separator=": ">
                         <WhatsAppIcon />
                     </WhatsappShareButton>
@@ -36,8 +51,10 @@ const ShareModal: FunctionComponent<Props> = ({ pollId }) => {
                     <EmailShareButton url={shareLink} subject={t("link.social.join")} title={t("link.social.join")}>
                         <MailOutlineIcon />
                     </EmailShareButton>
+                    <QrCode2OutlinedIcon onClick={() => setShowQr(true)} />
+                    <LinkOutlinedIcon onClick={copyToClipBoard} />
                 </ShareBar>
-                <Links>
+                <Links visible={showQR}>
                     <LinkButton active={true} onClick={() => navigate(`/decision/${pollId}`)}>
                         {t("create.links.start")}
                     </LinkButton>
@@ -45,6 +62,10 @@ const ShareModal: FunctionComponent<Props> = ({ pollId }) => {
                         {t("create.links.result")}
                     </LinkButton>
                 </Links>
+                <QRCodeWrapper visible={showQR}>
+                    <ArrowIcon onClick={() => setShowQr(false)} />
+                    <Image src={qrCodeBase64} alt="QR Code" />
+                </QRCodeWrapper>
             </Body>
         </Modal>
     );
