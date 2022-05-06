c1b7e4aac185ef1a30b7ceac105b8f9bd83ac348
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2022-05-06 12:51:45 +0200

    fix qrcode share

--- app/src/components/Share/Share.tsx
+++ app/src/components/Share/Share.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@apollo/client";
 import { GQLQuery } from "@app/graphql.generated";
+import { CustomModal } from "@components/Modals/QRModal";
 import { useSnack } from "@context/snackbar/useSnack";
 import QrCode2OutlinedIcon from "@mui/icons-material/QrCode2Outlined";
 import Fab from "@mui/material/Fab";
 import { getQRCode } from "@pages/DecisionPage/pollData.gql";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import { NavigatorShareWrapper, ShareWrapper } from "./Share.sc";
 import SocialShare from "./SocialShare";
@@ -19,6 +20,14 @@ const Share: FunctionComponent<Props> = ({ url }) => {
     const qrCodeData = useQuery<GQLQuery>(getQRCode, { variables: { data: { shareLink: url } } });
     const qrCodeBase64 = qrCodeData.data?.getQRCode.id;
 
+    const [openModal, setOpenModal] = useState(false);
+    const handleClickOpenModal = () => {
+        setOpenModal(true);
+    };
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
     let imageReady = false;
     if (typeof qrCodeBase64 === "string") {
         imageReady = true;
@@ -32,31 +41,31 @@ const Share: FunctionComponent<Props> = ({ url }) => {
         });
     };
 
-    const navigatorShareQrCode = () => {
-        if (imageReady) {
-            fetch(qrCodeBase64 as string)
-                .then((res) => res.blob())
-                .then((blob) => {
-                    const qrCode = new File([blob], "Dast-Qr-Code.png", { type: "image/png" });
+    // const navigatorShareQrCode = () => {
+    //     if (imageReady) {
+    //         fetch(qrCodeBase64 as string)
+    //             .then((res) => res.blob())
+    //             .then((blob) => {
+    //                 const qrCode = new File([blob], "Dast-Qr-Code.png", { type: "image/png" });
 
-                    if (navigator.canShare != undefined && navigator.canShare({ files: [qrCode] })) {
-                        navigator
-                            .share({
-                                title: "Dast - Decide fast",
-                                text: "Scan the QR code and joint the decision!",
-                                files: [qrCode],
-                            })
-                            .catch(() => {
-                                setSnack({
-                                    message: "Browser does not support sharing files",
-                                    severity: "error",
-                                    open: true,
-                                });
-                            });
-                    }
-                });
-        }
-    };
+    //                 if (navigator.canShare != undefined && navigator.canShare({ files: [qrCode] })) {
+    //                     navigator
+    //                         .share({
+    //                             title: "Dast - Decide fast",
+    //                             text: "Scan the QR code and joint the decision!",
+    //                             files: [qrCode],
+    //                         })
+    //                         .catch(() => {
+    //                             setSnack({
+    //                                 message: "Browser does not support sharing files",
+    //                                 severity: "error",
+    //                                 open: true,
+    //                             });
+    //                         });
+    //                 }
+    //             });
+    //     }
+    // };
 
     let ShareButton = (
         <ShareWrapper>
@@ -70,9 +79,12 @@ const Share: FunctionComponent<Props> = ({ url }) => {
                 <Fab size="small" onClick={navigatorShare}>
                     <ShareIcon></ShareIcon>
                 </Fab>
-                <Fab size="small" onClick={navigatorShareQrCode}>
+                <Fab size="small" onClick={handleClickOpenModal}>
                     <QrCode2OutlinedIcon fontSize="medium"></QrCode2OutlinedIcon>
                 </Fab>
+                <CustomModal openModal={openModal} handleClose={handleCloseModal}>
+                    {imageReady && <img src={qrCodeBase64}></img>}
+                </CustomModal>
             </NavigatorShareWrapper>
         );
     }
