87167eb63ef1a413f9d3e10c77cc20c832572126
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2022-05-17 20:27:24 +0200

    add integration for getting and creating pollstest

--- api/src/polls/polls.resolver.e2e.spec.ts
+++ api/src/polls/polls.resolver.e2e.spec.ts
@@ -1,7 +1,12 @@
+import { getRepositoryToken, MikroOrmModule } from "@mikro-orm/nestjs";
+import { EntityRepository } from "@mikro-orm/postgresql";
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "@src/app.module";
 import request from "supertest";
+
+import { Poll } from "./entities/poll.entity";
+import { PollsService } from "./polls.service";
 
 describe("PollsResolver - e2e", () => {
     const graphqlEndpoint = "/graphql";
@@ -9,10 +14,12 @@ describe("PollsResolver - e2e", () => {
     let currentUser: { id: string; token: string; refreshToken: string; name: string };
     let query: string;
     let createPollData: { title: string; type: string; owner: string };
+    let repository: EntityRepository<Poll>;
+    let service: PollsService;
 
     beforeEach(async () => {
         const appModule: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
+            imports: [AppModule, MikroOrmModule.forFeature([Poll])],
         }).compile();
 
         const appRef = appModule.createNestApplication();
@@ -24,6 +31,8 @@ describe("PollsResolver - e2e", () => {
 
         currentUser = userResponse.body.data.addUser;
         createPollData = { title: "Beer or Wine?", type: "BINARY", owner: currentUser.id };
+        repository = await app.get(getRepositoryToken(Poll));
+        service = appRef.get<PollsService>(PollsService);
     });
 
     afterAll(async () => {
@@ -34,88 +43,86 @@ describe("PollsResolver - e2e", () => {
         beforeEach(() => {
             query = `mutation{addPoll(data:{title:"${createPollData.title}", type:${createPollData.type}, owner: "${createPollData.owner}"}){id, title, owner{id}, type}}`;
         });
-        it("should not create a new poll if unauthorized", async () => {
-            const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).send({
-                query,
-            });
 
-            expect(body.errors).toBeTruthy();
-            expect(body.errors.some((e: { message: string }) => e.message === "Forbidden resource")).toBeTruthy();
-            expect(body.data?.addPoll).toBeFalsy();
+        describe("unauthorized", () => {
+            it("should not create a new poll", async () => {
+                const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).send({
+                    query,
+                });
+
+                expect(body.errors).toBeTruthy();
+                expect(body.errors.some((e: { message: string }) => e.message === "Forbidden resource")).toBeTruthy();
+                expect(body.data?.addPoll).toBeFalsy();
+            });
         });
 
-        it("should create a new poll authorized", async () => {
+        describe("authorized with Bearer token", () => {
+            it("should be able to create a new poll", async () => {
+                const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).set("Authorization", `Bearer ${currentUser.token}`).send({
+                    query,
+                });
+
+                expect(body.data.addPoll).toEqual(
+                    expect.objectContaining({ title: createPollData.title, type: createPollData.type, owner: { id: createPollData.owner } }),
+                );
+            });
+            it("persists created poll in the database with correct data", async () => {
+                const serviceSpy = jest.spyOn(service, "create");
+                const repoSpy = jest.spyOn(repository, "create");
+
+                const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).set("Authorization", `Bearer ${currentUser.token}`).send({
+                    query,
+                });
+
+                const databaseEntity = await repository.findOne(body.data.addPoll.id);
+
+                expect(serviceSpy).toHaveBeenCalled();
+                expect(repoSpy).toHaveBeenCalled();
+                expect(databaseEntity).not.toBe(null);
+                expect(databaseEntity?.title).toEqual(createPollData.title);
+                expect(databaseEntity?.owner?.id).toEqual(createPollData.owner);
+            });
+        });
+    });
+
+    describe("[Query] getPoll", () => {
+        let createMutation: string;
+        let pollId: string;
+
+        beforeEach(async () => {
+            createMutation = `mutation{addPoll(data:{title:"${createPollData.title}", type:${createPollData.type}, owner: "${createPollData.owner}"}){id, title, owner{id}, type}}`;
             const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).set("Authorization", `Bearer ${currentUser.token}`).send({
-                query,
+                query: createMutation,
             });
 
-            expect(body.data.addPoll).toEqual(
-                expect.objectContaining({ title: createPollData.title, type: createPollData.type, owner: { id: createPollData.owner } }),
-            );
+            pollId = body.data.addPoll.id;
+            query = `query{getPoll(data:{pollId: "${pollId}"}){id, title, type, owner{id}}}`;
         });
 
-        // it("should not create a new poll if wrong Bearer token given");
+        describe("unauthorized", () => {
+            it("should not get poll", async () => {
+                const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).send({
+                    query,
+                });
 
-        // it("should get a single customer by id", () => {
-        //     let customer;
-        //     return request(app.getHttpServer())
-        //         .post(graphqlEndpoint)
-        //         .send({
-        //             query: 'mutation {createCustomer(name: "John Doe", email: "john.doe@example.com", phone: "145677312965", address: "123 Road, Springfied, MO") {address name id phone email}}',
-        //         })
-        //         .expect(200)
-        //         .expect((res) => {
-        //             customer = res.body.data.createCustomer;
-        //         })
-        //         .then(() =>
-        //             request(app.getHttpServer())
-        //                 .post(gql)
-        //                 .send({
-        //                     query: `{customer(id: "${customer.id}") {address name id phone email}}`,
-        //                 })
-        //                 .expect(200)
-        //                 .expect((res) => {
-        //                     expect(res.body.data.customer).toEqual({
-        //                         id: customer.id,
-        //                         address: customer.address,
-        //                         name: customer.name,
-        //                         phone: customer.phone,
-        //                         email: customer.email,
-        //                     });
-        //                 }),
-        //         );
-        // });
+                expect(body.errors).toBeTruthy();
+                expect(body.errors.some((e: { message: string }) => e.message === "Forbidden resource")).toBeTruthy();
+                expect(body.data?.getPoll).toBeFalsy();
+            });
+        });
+        describe("authorized", () => {
+            it("should get right poll from database", async () => {
+                const serviceSpy = jest.spyOn(service, "findPoll");
+                const repoSpy = jest.spyOn(repository, "findOneOrFail");
+                const { body } = await request(app.getHttpServer()).post(graphqlEndpoint).set("Authorization", `Bearer ${currentUser.token}`).send({
+                    query,
+                });
 
-        // it("should retrieve all customer data", async () => {
-        //     const data = [
-        //         {
-        //             name: "John Doe",
-        //             email: "john.doe@example.com",
-        //             phone: "145677312965",
-        //             address: "123 Road, Springfied, MO",
-        //         },
-        //         {
-        //             name: "Jane Doe",
-        //             email: "jane.doe@example.com",
-        //             phone: "145677312900",
-        //             address: "456 Road, Springfied, MO",
-        //         },
-        //     ];
-        //     const connection = await getConnection();
-        //     data.map(async (item) => {
-        //         await connection.createQueryBuilder().insert().into(CustomerModel).values(item).execute();
-        //     });
-
-        //     request(app.getHttpServer())
-        //         .post(gql)
-        //         .send({
-        //             query: `{customers() {address name phone email}}`,
-        //         })
-        //         .expect(200)
-        //         .expect((res) => {
-        //             expect(res.body.data.customers.length).toEqual(data.length);
-        //             expect(res.body.data.customers[0]).toEqual(data[0]);
-        //         });
-        // });
+                expect(serviceSpy).toHaveBeenCalled();
+                expect(repoSpy).toHaveBeenCalled();
+                expect(body.data?.getPoll).not.toBeFalsy();
+                expect(body.data?.getPoll?.id).toEqual(pollId);
+            });
+        });
     });
 });
